c3431b7954a2ea70e114407f09fcc571d24a14e3
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-27 19:59:08 -0300

    feat: added route delete, create e list

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,22 +1,45 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import CreateTransactionService from '../services/CreateTransactionService';
+import DeleteTransactionService from '../services/DeleteTransactionService';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
-// import DeleteTransactionService from '../services/DeleteTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getCustomRepository(TransactionsRepository);
+  const transactions = await transactionsRepository.find();
+  const balance = await transactionsRepository.getBalance();
+
+  return response.json({ transactions, balance });
 });
 
 transactionsRouter.post('/', async (request, response) => {
+  const { title, type, value, category } = request.body;
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    category,
+    type,
+    value,
+  });
+
+  return response.json(transaction);
   // TODO
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
+  const { id } = request.params;
   // TODO
+  const deleteTransaction = new DeleteTransactionService();
+  await deleteTransaction.execute({
+    id,
+  });
+
+  return response.status(204).json();
 });
 
 transactionsRouter.post('/import', async (request, response) => {
