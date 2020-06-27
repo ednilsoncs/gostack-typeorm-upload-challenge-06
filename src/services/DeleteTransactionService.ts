0356509fb828b535c715a6360e4b57a17bcba797
ednilsoncs <ednilsoncs@dcomp.ufs.br>
2020-06-27 19:58:05 -0300

    feat: added Service to delete trasaction

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,7 +1,22 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import AppError from '../errors/AppError';
+
+interface Request {
+  id: string;
+}
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
+  public async execute({ id }: Request): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const checkIfTransactionsExists = await transactionsRepository.findOne(id);
+
+    if (!checkIfTransactionsExists) {
+      throw new AppError('Transaction not found', 404);
+    }
+
+    await transactionsRepository.delete(id);
     // TODO
   }
 }
